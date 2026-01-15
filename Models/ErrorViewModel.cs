a4ffbcbf05def241229cea1b90e9c099fb6e72d2
guiferrao <ferraogui@hotmail.com>
2026-01-15 20:19:13 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SistemaDeEstoque.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
