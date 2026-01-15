4406305dfd0a1e26df269ef18ffa685eb7adc235
guiferrao <ferraogui@hotmail.com>
2026-01-15 20:40:43 -0300

    Criação dos models e conexão com o banco

---
+++ Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaDeEstoque.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public List<Produto> Produtos { get; set; }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaDeEstoque.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
